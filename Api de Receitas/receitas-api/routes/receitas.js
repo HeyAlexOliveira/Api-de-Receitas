ea2f0fa973efa5ec9b4668fdb9e36e11df71b189
HeyAlexOliveira <135489596+HeyAlexOliveira@users.noreply.github.com>
2025-03-08 01:32:32 -0300

    receitas.js

--- Api de Receitas/receitas-api/routes/receitas.js
+++ Api de Receitas/receitas-api/routes/receitas.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root', 
-    password: 'alexftc', 
+    password: 'SuaSenhaMySql', 
     database: 'receitasDB'
 });
 
